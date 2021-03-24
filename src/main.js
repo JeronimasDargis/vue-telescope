69d1827982d9a6678a52db2ccabb8ed44a3dbfbe
Jeronimas Dargis <jeronimasd@gmail.com>
2021-03-24 11:16:19 +0200

    feat: creating simple directive for text truncation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import telescope from "../lib";
+
+const app = createApp(App);
+
+app.use(telescope);
+
+app.mount("#app");
