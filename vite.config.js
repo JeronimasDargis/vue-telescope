4269c6c208d98fa836d65fd4521218b999502c23
Jeronimas Dargis <jeronimasd@gmail.com>
2021-03-31 09:48:27 +0300

    feat: adding lib vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+const path = require("path");
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+module.exports = {
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.js"),
+      name: "MyLib",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+};
